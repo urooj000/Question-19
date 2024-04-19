80bd4733e1a543b0a8ce1bc8a5c27e42189d274c
urooj22 <160038688+urooj000@users.noreply.github.com>
2024-04-19 22:07:10 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,4 @@
+"use strict";
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+let placeToVisit = ["Makkah", "Sydney", "Dubai", "Paris", "London"];
+console.log(`Number of dinner guests: ${placeToVisit.length}`);
---
+++ main.ts
@@ -0,0 +1,5 @@
+// Dinner Guests: Working with one of the programs from Exercises 14 through 18, print a message indicating the number of people you are inviting to dinner.
+
+let placeToVisit: string[]  = ["Makkah", "Sydney", "Dubai", "Paris", "London"];
+
+console.log(`Number of dinner guests: ${placeToVisit.length}`);
